27b0bdaa173221d5a5e8f7b997cfeb8794d39fc9
Sebastian Matheu Martinez <sematheu@gmail.com>
2019-07-17 00:46:33 +0200

    Diagonal controls.

--- input_handlers.py
+++ input_handlers.py
@@ -4,14 +4,22 @@ import tcod.event as event
 
 def handle_keys(ev):
     # Movement keys
-    if ev.sym == event.K_UP:
+    if ev.sym == event.K_UP or ev.sym == event.K_k:
         return {'move': (0, -1)}
-    elif ev.sym == event.K_DOWN:
+    elif ev.sym == event.K_DOWN or ev.sym == event.K_j:
         return {'move': (0, 1)}
-    elif ev.sym == event.K_LEFT:
+    elif ev.sym == event.K_LEFT or ev.sym == event.K_h:
         return {'move': (-1, 0)}
-    elif ev.sym == event.K_RIGHT:
+    elif ev.sym == event.K_RIGHT or ev.sym == event.K_l:
         return {'move': (1, 0)}
+    elif ev.sym == event.K_y:
+        return {'move': (-1, -1)}
+    elif ev.sym == event.K_u:
+        return {'move': (1, -1)}
+    elif ev.sym == event.K_b:
+        return {'move': (-1, 1)}
+    elif ev.sym == event.K_n:
+        return {'move': (1, 1)}
     elif ev.sym == event.K_SPACE:
         return {'map': True}
 

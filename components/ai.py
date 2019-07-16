074bf1c394d09e27d154e395fc09859257ef4df3
Sebastian Matheu Martinez <sematheu@gmail.com>
2019-07-17 00:26:22 +0200

    Fighter and AI components created.

---
+++ components/ai.py
@@ -0,0 +1,3 @@
+class BasicMonster:
+    def take_turn(self):
+        print('The {0} wonders then it will get a turn'.format(self.owner.name))
--- entity.py
+++ entity.py
@@ -1,11 +1,19 @@
 class Entity:
-    def __init__(self, x, y, char, color, name, blocks=False):
+    def __init__(self, x, y, char, color, name, blocks=False, fighter=None, ai=None):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
         self.name = name
         self.blocks = blocks
+        self.fighter = fighter
+        self.ai = ai
+
+        if self.fighter:
+            self.fighter.owner = self
+
+        if self.ai:
+            self.ai.owner = self
 
     def move(self, dx, dy):
         self.x += dx
@@ -16,4 +24,4 @@ def get_blocking_entities_at_location(entities, destination_x, destination_y):
         if entity.blocks and entity.x == destination_x and entity.y == destination_y:
             return entity
 
-    return None 
+    return None
